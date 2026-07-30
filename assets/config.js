68698047791010027c95686460a1f6c469338612
NOLAKeith <keith.nunez@gmail.com>
2026-07-30 12:52:00 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,7 @@
 window.CHILL_FEST_CONFIG = {
   registrationEndpoint: "https://script.google.com/macros/s/AKfycbyZcGmlGcKcx62Ez0e14sB_oTgtiX31YRF4p8OO10DgVTjgvKXh2XpPx4hxPwxrkv_N/exec",
   showWhosComing: true,
+  showSchedule: false,
   showTournamentPages: true,
   showSponsors: true,
   tournament: {
